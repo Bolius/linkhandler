35b24fdf2e36549e22adddf69f52053b7f26f5b4
Einar Gislason <egi@bolius.dk>
2015-02-06 14:42:51 +0000

    Fix if statement

--- Classes/Record/RecordTree.php
+++ Classes/Record/RecordTree.php
@@ -102,10 +102,10 @@ class RecordTree extends \TYPO3\CMS\Backend\Tree\View\BrowseTreeView {
 				$arrCol = '<td></td>';
 			}
 			$addPassOnParams = $this->getaddPassOnParams();
-			
+
             if(
 	            $this->browselistObj->thisConfig['tx_linkhandler.'][$this->browselistObj->act.'.']['listTables'] != 'pages' OR
-	            $this->browselistObj->thisConfig['tx_linkhandler.'][$this->browselistObj->act.'.']['treatAsRecords'] == 0
+	            $this->browselistObj->thisConfig['tx_linkhandler.'][$this->browselistObj->act.'.']['treatAsRecords'] != 1
             ) {
                 $aOnClick = 'return jumpToUrl(\'' . $this->thisScript . '?act=' .
                 $GLOBALS['SOBE']->browser->act .
